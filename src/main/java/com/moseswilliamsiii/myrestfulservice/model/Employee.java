51284eb31d1241e51c84aefaddf3a4011d664baf
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-18 11:57:43 -0400

    Employee entity

---
+++ src/main/java/com/moseswilliamsiii/myrestfulservice/model/Employee.java
@@ -0,0 +1,37 @@
+package com.moseswilliamsiii.myrestfulservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private String id;
+    private String name;
+
+    public Employee(){}
+
+    public Employee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
